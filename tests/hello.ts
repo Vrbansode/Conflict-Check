a1af15f9342096ad22c2a5b7110e016dbf936e08
Vrbansode <vinayak.vrb@gmail.com>
2025-12-18 13:57:42 +0530

    Change message in feature branch

--- tests/hello.ts
+++ tests/hello.ts
@@ -4,3 +4,12 @@ function greet(name:string): string{
 
 const messgae: string =greet("World");
 console.log(messgae);
+
+const pulltest: string =greet("pull-test");
+console.log(pulltest);
+
+const conflict: string =greet("conflict-test");
+console.log(conflict);
+
+const conflict2: string =greet("conflict-test2");
+console.log(conflict2);
